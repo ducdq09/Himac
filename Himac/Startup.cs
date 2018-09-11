6a6867e66b6ee5b640c7740bcc96aea697fe07d5
Đỗ Quảng Đức <ducdq.hy@hotmail.com>
2018-09-11 10:19:43 +0700

    test

--- Himac/Startup.cs
+++ Himac/Startup.cs
@@ -15,6 +15,7 @@ namespace Himac
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //add service
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
